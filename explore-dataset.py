b8e58926a5dbc6221bb14ca5ba5906a64362338e
ruru-eng <alexis.idzakovic@gmail.com>
2025-07-15 09:31:24 +0200

    Initial commit

---
+++ explore-dataset.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv("../survey_data.csv")
+
+# display the top 5 rows and columns of the dataset 
+print(df.head())
+
+# print the number of rows and columns in the dataset
+print(df.shape)
+
+# print datatype of each column
+print(df.dtypes)
+
+# print mean age of the survey participitants - use the most frequent one
+print(df["Age"].value_counts())
+
+# print how many unique countries are there in the Country column
+print(len(df["Country"].unique()))
