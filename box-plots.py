aeb05c0306303a2a2ccd6f67140c4b2042e719a1
ruru-eng <alexis.idzakovic@gmail.com>
2025-07-22 14:41:35 +0200

    Initial commit

--- box-plots.py
+++ box-plots.py
@@ -4,4 +4,9 @@ import numpy as np
 import seaborn as sns
 import sqlite3
 
+conn = sqlite3.connect('survey-results-public.sqlite')
 
+
+QUERY = "SELECT COUNT(*) FROM main"
+df = pd.read_sql_query(QUERY, conn)
+print(df)
